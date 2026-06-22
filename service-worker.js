09e2a78d37fad314bae36da7c783c2687e920a9f
Roms66200 <b9c78rw98j@privaterelay.appleid.com>
2026-06-22 10:13:35 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,13 +1,1 @@
-const CACHE_NAME = "ucdp-pwa-v3";
-const FILES = ["./", "./index.html", "./manifest.json"];
-self.addEventListener("install", event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES)));
-  self.skipWaiting();
-});
-self.addEventListener("activate", event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME ? caches.delete(k) : null))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
-});
+const CACHE_NAME="ucdp-pwa-v4";const FILES=["./","./index.html?v=4","./manifest.json"];self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES)));self.skipWaiting()});self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));self.clients.claim()});self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{let copy=r.clone();caches.open(CACHE_NAME).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)))})
